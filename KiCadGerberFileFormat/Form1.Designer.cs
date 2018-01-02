3d3dc10af038fd7dc7c6fa957eb34550bfce02de
Kotakku <Kotakkucu@gmail.com>
2018-01-02 21:20:56 +0900

    First commit

---
+++ KiCadGerberFileFormat/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace KiCadGerberFileFormat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.FolderBrowseButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.FormatButton = new System.Windows.Forms.Button();
+            this.zipCheckBox = new System.Windows.Forms.CheckBox();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(435, 22);
+            this.fileNameTextBox.TabIndex = 0;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 362);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(540, 25);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 19);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(33, 20);
+            this.toolStripStatusLabel.Text = "abc";
+            // 
+            // FolderBrowseButton
+            // 
+            this.FolderBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FolderBrowseButton.Location = new System.Drawing.Point(453, 12);
+            this.FolderBrowseButton.Name = "FolderBrowseButton";
+            this.FolderBrowseButton.Size = new System.Drawing.Size(75, 25);
+            this.FolderBrowseButton.TabIndex = 2;
+            this.FolderBrowseButton.Text = "参照";
+            this.FolderBrowseButton.UseVisualStyleBackColor = true;
+            this.FolderBrowseButton.Click += new System.EventHandler(this.FolderBrowseButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.AcceptsTab = true;
+            this.logTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logTextBox.Location = new System.Drawing.Point(12, 53);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(516, 270);
+            this.logTextBox.TabIndex = 3;
+            // 
+            // FormatButton
+            // 
+            this.FormatButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FormatButton.Location = new System.Drawing.Point(428, 329);
+            this.FormatButton.Name = "FormatButton";
+            this.FormatButton.Size = new System.Drawing.Size(100, 30);
+            this.FormatButton.TabIndex = 4;
+            this.FormatButton.Text = "変換";
+            this.FormatButton.UseVisualStyleBackColor = true;
+            this.FormatButton.Click += new System.EventHandler(this.FormatButton_Click);
+            // 
+            // zipCheckBox
+            // 
+            this.zipCheckBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.zipCheckBox.AutoSize = true;
+            this.zipCheckBox.Checked = true;
+            this.zipCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.zipCheckBox.Location = new System.Drawing.Point(12, 336);
+            this.zipCheckBox.Name = "zipCheckBox";
+            this.zipCheckBox.Size = new System.Drawing.Size(217, 19);
+            this.zipCheckBox.TabIndex = 5;
+            this.zipCheckBox.Text = "ファイル名変換後.zipに圧縮する";
+            this.zipCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 387);
+            this.Controls.Add(this.zipCheckBox);
+            this.Controls.Add(this.FormatButton);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.FolderBrowseButton);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.Button FolderBrowseButton;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Button FormatButton;
+        private System.Windows.Forms.CheckBox zipCheckBox;
+    }
+}
+
---
+++ KiCadGerberFileFormat/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace KiCadGerberFileFormat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FolderBrowseButton_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = @"D:\Users\Owner\Documents\KiCad";
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                //Console.WriteLine(fbd.SelectedPath);
+                fileNameTextBox.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void FormatButton_Click(object sender, EventArgs e)
+        {
+            string parentFolderName = @fileNameTextBox.Text;
+            string folderName = @fileNameTextBox.Text + "\\exportZip_Log";
+
+            if (Directory.Exists(parentFolderName))
+            {
+                //zip作成の時の名前に使う
+                string baseFileName = "";
+
+                logTextBox.Text = "";
+                //指定フォルダの中にフォルダを作り中身をコピー、そっちで作業する
+                logTextBox.AppendText("parent Folder: " + parentFolderName + "\r\n");
+                logTextBox.AppendText("log    Folder: " + folderName + "\r\n\r\n");
+
+                DirectoryCopy(parentFolderName, folderName, false);
+
+                //フォルダにあるファイルの数を取得
+                int fileCount = Directory.GetFiles(folderName, "*", SearchOption.TopDirectoryOnly).Length;
+
+                if(fileCount > 0)
+                {
+                    logTextBox.AppendText("at '" + folderName + "'\r\n  ファイルは" + fileCount + "個です。\r\n\r\n");
+                }
+                else
+                {
+                    logTextBox.AppendText("ファイルが存在しません。\r\n");
+                    return;
+                }
+
+                //全てのファイル名を取得
+                string[] past_fileNames = new string[fileCount];
+                string[] fileNames = new string[fileCount];
+
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(folderName);
+
+                IEnumerable<System.IO.FileInfo> files = di.EnumerateFiles("*", System.IO.SearchOption.AllDirectories);
+                int i = 0;
+                foreach (System.IO.FileInfo f in files)
+                {
+                    past_fileNames[i] = Path.GetFileName(f.FullName);
+                    fileNames[i++] = Path.GetFileName(f.FullName);
+                }
+
+
+                //ファイル名の最低限の部分の長さを数える
+                int baseFileNameLength = -1;
+                for (i = 0; i < fileCount; i++)
+                {
+                    if(baseFileNameLength == -1)
+                    {
+                        baseFileNameLength = fileNames[i].LastIndexOf('.');
+                    }
+                    else
+                    {
+                        if(fileNames[i].LastIndexOf('.') < baseFileNameLength)
+                        {
+                            baseFileNameLength = fileNames[i].LastIndexOf('.');
+                        }
+                    }
+                }
+                baseFileName = fileNames[0].Substring(0, baseFileNameLength);
+                //logTextBox.AppendText("\r\n");
+
+                //各ファイルの変換後の名前を出す
+                for (i = 0; i < fileCount; i++)
+                {
+                    // '-'より後を消す
+                    logTextBox.AppendText(fileNames[i] + "\r\n");
+                    int periodPosition = fileNames[i].LastIndexOf('.');
+                    int hyphenPosition = fileNames[i].LastIndexOf('-');
+                    
+                    if(hyphenPosition >= baseFileNameLength)
+                    {
+                        fileNames[i] = fileNames[i].Remove(hyphenPosition, periodPosition - hyphenPosition);
+                    }
+                    logTextBox.AppendText("     ->  " + fileNames[i] + "\r\n");
+
+
+                    //拡張子を変換する
+                    //ドリルファイル(.drl)を(.txt)に変換
+                    periodPosition = fileNames[i].LastIndexOf('.');
+                    if (fileNames[i].Substring(baseFileNameLength + 1) == "drl")
+                    {
+                        fileNames[i] = fileNames[i].Remove(periodPosition + 1, 3);
+                        fileNames[i] += "txt";
+                        logTextBox.AppendText("    *->  " + fileNames[i] + "\r\n");
+                    }
+                    //基板外形データ(.gm1)を(.gko)に変換
+                    if (fileNames[i].Substring(baseFileNameLength + 1) == "gm1")
+                    {
+                        fileNames[i] = fileNames[i].Remove(periodPosition + 1, 3);
+                        fileNames[i] += "gko";
+                        logTextBox.AppendText("    *->  " + fileNames[i] + "\r\n");
+                    }
+                    logTextBox.AppendText("\r\n");
+                }
+
+                //ファイル名を変換
+                for (i = 0; i < fileCount; i++)
+                {
+                    try
+                    {
+                        if(past_fileNames[i] != fileNames[i])
+                        {
+                            File.Copy(folderName + "\\" + past_fileNames[i], folderName + "\\" + fileNames[i], true);
+                            File.Delete(folderName + "\\" + past_fileNames[i]);
+                        }
+                    }
+                    catch(Exception a)
+                    {
+                        logTextBox.AppendText(a + "\r\n");
+                    }
+
+                }
+
+                //余分なファイルがあれば削除
+                for(i = 0; i < fileCount; i++)
+                {
+                    int periodPosition = fileNames[i].LastIndexOf('.');
+                    string fileExtension = fileNames[i].Substring(periodPosition + 1).ToLower();
+                    string[] extensions = { "gbl", "gbo", "gbs", "gko", "gtl", "gto", "gts", "txt" };
+                    bool isDelete = true;
+
+                    //logTextBox.AppendText(fileExtension + "\r\n");
+                    foreach (string ext in extensions)
+                    {
+                        if (fileExtension == ext)
+                            isDelete = false;
+                    }
+
+                    if(isDelete)
+                    {
+                        //logTextBox.AppendText("DELETE:" + fileNames[i] + "\r\n");
+                        File.Delete(folderName + "\\" + fileNames[i]);
+                    }
+                }
+
+                //フォルダの中身を(.zip)にまとめる
+                if (zipCheckBox.Checked == true)
+                {
+                    try
+                    {
+                        string zipPath = parentFolderName + "\\" + baseFileName + ".zip";
+
+                        //同じ名前のzipファイルが存在する場合、一度消す
+                        if(File.Exists(zipPath))
+                        {
+                            File.Delete(zipPath);
+                        }
+                        ZipFile.CreateFromDirectory(folderName, zipPath);
+                        logTextBox.AppendText("\r\n" + baseFileName + ".zipを作成しました\r\n");
+                    }
+                    catch (Exception a)
+                    {
+                        logTextBox.AppendText(a + "\r\n");
+                    }
+                }
+            }
+            else
+            {
+                logTextBox.AppendText("'" + folderName + "'は存在しません。\r\n");
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+    }
+}
